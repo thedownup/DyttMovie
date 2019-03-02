e03c0d56e9ed91b64b41fed51a7dd9d7fe2dbdfa
zjt <632443784@qq.com>
2019-03-02 16:18:29 +0800

    1

--- movie-service-web/src/main/java/com/movie/ipfilter/IpFilter.java
+++ movie-service-web/src/main/java/com/movie/ipfilter/IpFilter.java
@@ -22,42 +22,39 @@ import com.movie.pojo.IPMessage;
 @Component
 public class IpFilter {
 
-	private Logger logger = Logger.getLogger(IpFilter.class);
-	
-	@Autowired
-	private HttpManager httpManager;
-	
-	/**
-	 * 判断此代理IP是否稳固
-	 * @param ipMessage
-	 * @return
-	 */
-	public boolean isTable(IPMessage ipMessage){
-		CloseableHttpClient httpClient = null;
-		CloseableHttpResponse response = null;
-		String url = "http://my.163.com/";
-		Lock lock = null;
-			try {
-				lock = new ReentrantLock();
-				lock.lock();
-				httpClient = httpManager.createHttpClient(5000,new HttpHost(ipMessage.getIpAdress(), ipMessage.getPort()));
-				try {
-					response = httpClient.execute(new HttpGet(url));
-				} catch (IOException e) {
-					logger.error(e.getLocalizedMessage());
-					return false;
-				}
-			} finally {
-				lock.unlock();
-			}
-			if (response == null || response.equals("")) {
-				return false;
-			}
-			if (response.getStatusLine().getStatusCode() == 200) {
-				return true;
-			}else {
-				return false;
-			}
+    private Logger logger = Logger.getLogger(IpFilter.class);
 
-	}
+    @Autowired
+    private HttpManager httpManager;
+
+    /**
+     * 判断此代理IP是否稳固
+     *
+     * @param ipMessage
+     * @return
+     */
+    public boolean isTable(IPMessage ipMessage) {
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        String url = "http://my.163.com/";
+        Lock lock = null;
+        try {
+            lock = new ReentrantLock();
+            lock.lock();
+            httpClient = httpManager.createHttpClient(5000, new HttpHost(ipMessage.getIpAdress(), ipMessage.getPort()));
+            try {
+                response = httpClient.execute(new HttpGet(url));
+            } catch (IOException e) {
+                logger.error(e.getLocalizedMessage());
+                return false;
+            }
+        } finally {
+            lock.unlock();
+        }
+        if (response == null) {
+            return false;
+        }
+        return response.getStatusLine().getStatusCode() == 200;
+
+    }
 }
